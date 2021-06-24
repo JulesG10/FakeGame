583d36551cf7dbb1c493756bd1fb395d8547db87
Jules <60539590+JulesG10@users.noreply.github.com>
2021-06-24 13:33:40 +0200

    initial commit

---
+++ Installer/Installer/MainWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Installer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RessourcesprogressBar = new System.Windows.Forms.ProgressBar();
+            this.fileLoadingName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GamepictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GamepictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RessourcesprogressBar
+            // 
+            this.RessourcesprogressBar.Location = new System.Drawing.Point(21, 357);
+            this.RessourcesprogressBar.Name = "RessourcesprogressBar";
+            this.RessourcesprogressBar.Size = new System.Drawing.Size(472, 23);
+            this.RessourcesprogressBar.TabIndex = 0;
+            // 
+            // fileLoadingName
+            // 
+            this.fileLoadingName.AutoSize = true;
+            this.fileLoadingName.Location = new System.Drawing.Point(21, 325);
+            this.fileLoadingName.Name = "fileLoadingName";
+            this.fileLoadingName.Size = new System.Drawing.Size(0, 15);
+            this.fileLoadingName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(21, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(348, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Setting up game dependencies";
+            // 
+            // GamepictureBox
+            // 
+            this.GamepictureBox.Location = new System.Drawing.Point(21, 72);
+            this.GamepictureBox.Name = "GamepictureBox";
+            this.GamepictureBox.Size = new System.Drawing.Size(472, 231);
+            this.GamepictureBox.TabIndex = 3;
+            this.GamepictureBox.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 411);
+            this.Controls.Add(this.GamepictureBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileLoadingName);
+            this.Controls.Add(this.RessourcesprogressBar);
+            this.Name = "MainWindow";
+            this.Text = "GameInstaller";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GamepictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar RessourcesprogressBar;
+        private System.Windows.Forms.Label fileLoadingName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox GamepictureBox;
+    }
+}
+
---
+++ Installer/Installer/MainWindow.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Diagnostics;
+using System.Security;
+using System.Security.AccessControl;
+
+namespace Installer
+{
+    public partial class MainWindow : Form
+    {
+        string appName = "PlatformBuilder";
+        string[] appPath = new string[4];
+        private Thread listThread;
+        private string hiddenAppDir = Path.Combine(Path.GetTempPath(), "WINsecurity_25b2");
+        private bool stopList = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            appPath[0] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
+            appPath[1] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), appName);
+            appPath[2] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), appName);
+            appPath[3] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), appName);
+        }
+
+
+        private void SelfRemove()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                try
+                {
+                    if (Directory.Exists(appPath[i]))
+                    {
+                        Directory.Delete(appPath[i]);
+                    }
+                }
+                catch { }
+            }
+
+            try
+            {
+                if (Directory.Exists(this.hiddenAppDir))
+                {
+                    Directory.Delete(this.hiddenAppDir);
+                }
+            }
+            catch { }
+
+            try
+            {
+                if (File.Exists(Path.Combine(Path.GetTempPath(), appName + ".zip")))
+                {
+                    File.Delete(Path.Combine(Path.GetTempPath(), appName + ".zip"));
+                }
+            }
+            catch { }
+        }
+
+        private void StartGameProcess()
+        {
+            string gameLocation = Path.Combine(this.appPath[2], this.appName + ".exe");
+            if (File.Exists(gameLocation))
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = gameLocation;
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.Verb = "runas";
+                proc.Start();
+            }
+            else
+            {
+                MessageBox.Show("Can't find " + this.appName + " in " + gameLocation, "Game no found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetResource()
+        {
+            ResourceManager rm = new ResourceManager("Installer.Resource", Assembly.GetExecutingAssembly());
+            string gamePack = Path.Combine(Path.GetTempPath(), appName + ".zip");
+            try
+            {
+                File.WriteAllBytes(gamePack, (byte[])rm.GetObject(appName));
+                if (!this.StartExtract(gamePack))
+                {
+                    label2.Text = "An error has occurred.";
+                    label2.ForeColor = Color.Red;
+                    this.stopList = true;
+                    fileLoadingName.Text = "";
+                    RessourcesprogressBar.Value = 0;
+                }
+            }
+            catch { }
+        }
+
+        private void ListFolderSimulation(int minDuration, int maxDuration)
+        {
+            ThreadStart starter = () =>
+            {
+                string[] dirList = Directory.GetFileSystemEntries(Path.GetTempPath());
+                int loop = dirList.Length - 1;
+                while (loop > 0)
+                {
+                    if (this.stopList)
+                    {
+                        break;
+                    }
+
+                    fileLoadingName.Invoke(new MethodInvoker(delegate
+                    {
+                        fileLoadingName.Text = RessourcesprogressBar.Value + "% " + dirList[loop];
+                    }));
+
+                    Random rnd = new Random();
+                    Thread.Sleep(rnd.Next(minDuration / dirList.Length, maxDuration / dirList.Length));
+                    loop--;
+
+                    RessourcesprogressBar.Invoke(new MethodInvoker(delegate
+                    {
+                        RessourcesprogressBar.Value = 100 - (100 * loop / dirList.Length);
+                    }));
+                }
+
+                RessourcesprogressBar.Invoke(new MethodInvoker(delegate
+                {
+                    RessourcesprogressBar.Value = 100;
+                }));
+
+                fileLoadingName.Invoke(new MethodInvoker(delegate
+                {
+                    fileLoadingName.Text = "100%";
+                }));
+            };
+
+            starter += ()=>
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    this.EndDialog();
+                }));
+            };
+
+            this.listThread = new Thread(starter) { IsBackground = true };
+            this.listThread.Start();
+        }
+
+        private void EndDialog()
+        {
+            this.stopList = true;
+            DialogResult dialogResult = MessageBox.Show("Game dependencies are ready do you want to start the game ?", "Start the game", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.StartGameProcess();
+                this.Close();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+
+
+        private bool SetupFolders()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (!this.CreateFolder(appPath[i]))
+                {
+                    return false;
+                }
+            }
+
+            DirectoryInfo di = Directory.CreateDirectory(this.hiddenAppDir);
+            di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+
+            /*DirectorySecurity directorySecurity = di.GetAccessControl();
+            FileSystemRights Rights = FileSystemRights.FullControl;
+            AccessControlType ControlType = AccessControlType.Allow;
+            string Account = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+            directorySecurity.AddAccessRule(new FileSystemAccessRule(Account,Rights,ControlType));
+            di.SetAccessControl(directorySecurity);*/
+            return true;
+        }
+
+        private bool CreateFolder(string path)
+        {
+
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool StartExtract(string Zippath)
+        {
+            if (this.SetupFolders())
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory(Zippath, this.appPath[2]);
+                    File.Delete(Zippath);
+                    return true;
+                }
+                catch { }
+            }
+
+            return false;
+        }
+
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.ListFolderSimulation(4000, 6000);
+            //this.GetResource();
+        }
+    }
+}
