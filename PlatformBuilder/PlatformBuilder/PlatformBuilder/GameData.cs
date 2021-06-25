53e014dcdc87f4b210965b5d28b0d64168d55ea0
Jules <60539590+JulesG10@users.noreply.github.com>
2021-06-25 18:31:58 +0200

    PlaformBuilder

---
+++ PlatformBuilder/PlatformBuilder/PlatformBuilder/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatformBuilder.GameObjects
+{
+    public class GameData
+    {
+        public GameData()
+        {
+
+        }
+    }
+}
---
+++ PlatformBuilder/PlatformBuilder/PlatformBuilder/GameObjects/Camera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlatformBuilder.GameObjects
+{
+    public enum CameraStates
+    {
+        PointFixed,
+        ObjectCenterFocus,
+        ObjectFocus
+    }
+
+    public class Camera
+    {
+        public CameraStates states { get; set; } = CameraStates.ObjectCenterFocus;
+        public Vector2 size { get; private set; }
+        public Vector2 position { get; set; }
+
+        public Camera(Vector2 size, Vector2 position)
+        {
+            this.size = size;
+            this.position = position;
+        }
+
+        public bool isInView(Camera camera)
+        {
+            if (camera.position.X < this.position.X + this.size.X && camera.position.X + camera.size.X > this.position.X && camera.position.Y < this.position.Y + this.size.Y && camera.size.Y + camera.position.Y > this.position.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
